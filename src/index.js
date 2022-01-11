cb39fd9024cabf415a86b9ebf4ab8b0d2bf0ae31
skynetfrank <frank.uah@gmail.com>
2022-01-11 10:15:58 -0400

    deploy 10 enero 2022

--- src/index.js
+++ src/index.js
@@ -22,7 +22,6 @@ const menu = document.querySelectorAll('nav ul li');
 const menuLinks = document.querySelectorAll('nav ul li a');
 const sesion = document.getElementById('sesion');
 const formSesion = document.getElementById('form-sesion');
-const spinner = document.querySelector('.spinner');
 const logout = document.getElementById('logout-link');
 const forgotPassword = document.getElementById('forgot-pw');
 const seccionPacientes = document.getElementById('seccion__pacientes');
@@ -40,8 +39,7 @@ const nav = document.querySelector('.nav');
 const navOverlay = document.querySelector('.nav-overlay');
 const closeNav = document.querySelector('.close');
 const fechaAgenda = document.querySelector('.head > p');
-let nombreCita;
-var usuarioWeb = [];
+var usuariosWeb = [];
 
 //funcion para convertir fecha a formato DD-MM-AAAA
 function formatearFecha(nfecha) {
@@ -57,21 +55,6 @@ function convertirFecha(cfecha) {
   return year + '-' + month + '-' + day;
 }
 
-function autoCapital(cadena) {
-  return cadena.charAt(0).toUpperCase() + cadena.slice(1);
-}
-
-window.addEventListener('load', () => {
-  /*  spinner.style.display = 'none';
-  seccionInicio.style.display = 'none';
-  imgLogo.click();
-  console.log('Loaded window Auth:', auth?.currentUser);
-  //seccionPacientes.style.display = 'block';
-  menuLinks[0].style.color = 'lime';
-  infoContainer.style.display = 'none';
- */
-});
-
 onAuthStateChanged(auth, user => {
   if (user) {
     sesion.style.display = 'none';
@@ -81,7 +64,6 @@ onAuthStateChanged(auth, user => {
       link.style.pointerEvents = 'all';
       link.style.color = 'white';
     });
-    //    imgLogo.click();
     menuLinks[0].click();
     populateTabla();
     getUsuarios();
@@ -120,9 +102,7 @@ forgotPassword.addEventListener('click', e => {
       alert('Se ha enviado un correo de restablecimiento de contraseña al email: ' + email);
     })
     .catch(error => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
+      alert('Ocurrio un error!...' + error.message);
     });
 });
 
@@ -347,41 +327,6 @@ btnCerrarInfo.addEventListener('click', () => {
 
 const btnUpdateAgenda = document.getElementById('update-agenda');
 
-//FUNCION PARA DESPLEGAR AGENDA POP-UP
-(function () {
-  'use strict';
-  var items = document.querySelectorAll('.timeline li');
-
-  function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-
-  function callbackFunc() {
-    var items = document.querySelectorAll('.timeline li');
-    for (var i = 0; i < items.length; i++) {
-      if (isElementInViewport(items[i])) {
-        items[i].classList.add('in-view');
-      } else {
-        items[i].classList.remove('in-view');
-      }
-      if (i == 0) {
-        items[i].classList.add('in-view');
-      }
-    } //end of for
-  }
-  // listen for events
-  window.addEventListener('load', callbackFunc);
-  window.addEventListener('resize', callbackFunc);
-  window.addEventListener('scroll', callbackFunc);
-})();
-//FIN DE FUNCION PARA DESPLEGAR AGENDA POP-UP
-
 //INICIO DEL LISTENER PARA LA AGENDA
 menuLinks[1].addEventListener('click', () => {
   seccionInicio.style.display = 'none';
@@ -392,35 +337,21 @@ menuLinks[1].addEventListener('click', () => {
   menuLinks[0].style.color = 'white';
   menuLinks[2].style.color = 'white';
   horario();
+  populateAgenda();
+}); //FINDE LISTENER PARA AGENDA
+
+function populateAgenda() {
   let timeLista = document.getElementById('ul-timeline');
-  const usersRef = collection(db, 'users');
+  let fecha = convertirFecha(new Date());
+  const consultaAgenda = query(collection(db, 'citas'), where('fecha', '>=', fecha), orderBy('fecha', 'asc'));
+  const allCitas = onSnapshot(consultaAgenda, snapshot => {
+    //aqui se itera sobre el cursor resultado (snapshot)
+    timeLista.innerHTML = '';
+    snapshot.forEach(doc => {
+      let data = doc.data();
+      let found = usuariosWeb.find(p => p.id === data.paciente);
 
-  /* 
-  getDocs(usersRef)
-    .then(snapshot => {
-      snapshot.docs.forEach(paciente => {
-        let currentID = paciente.id;
-        let appObj = { ...paciente.data(), ['id']: currentID };
-        pacientex.push(appObj);
-        console.log('usuario agregado al array pacientesx');
-      });
-    })
-    .catch(error => {
-      console.log('Ocurrio un Error: ', error.message);
-    });
- */
-  const populateAgenda = () => {
-    let fecha = convertirFecha(new Date());
-    const consultaAgenda = query(collection(db, 'citas'), where('fecha', '>=', fecha), orderBy('fecha', 'asc'));
-    const allCitas = onSnapshot(consultaAgenda, snapshot => {
-      //aqui se itera sobre el cursor resultado (snapshot)
-      timeLista.innerHTML = '';
-      snapshot.forEach(doc => {
-        let data = doc.data();
-        let found = usuarioWeb.find(p => p.id === data.paciente);
-        console.log('Pacientex array:', usuarioWeb, 'found:', found);
-
-        let fila = `<li>
+      let fila = `<li>
           <div> 
                <span class="header-cita">           
                  <h1>Dia: ${formatearFecha(data.fecha)}</h1>
@@ -440,29 +371,24 @@ menuLinks[1].addEventListener('click', () => {
                <span id="id-cita-eliminar">${doc.id}</span>         
           </div>
           </li>`;
-        timeLista.innerHTML += fila;
+      timeLista.innerHTML += fila;
 
-        window.scroll(0, 1);
-        //seleccionar todos los botones eliminar cita
-        const allButtons = document.querySelectorAll('.btn-eliminar-cita');
-        //loop de botones de la tabla
+      //seleccionar todos los botones eliminar cita
+      const allButtons = document.querySelectorAll('.btn-eliminar-cita');
+      //loop de botones de la tabla
 
-        allButtons.forEach(boton => {
-          boton.addEventListener('click', e => {
-            let idCita = e.target.dataset.idcita;
-            deleteCita(idCita);
-          });
+      allButtons.forEach(boton => {
+        boton.addEventListener('click', e => {
+          let idCita = e.target.dataset.idcita;
+          deleteCita(idCita);
         });
-
-        //fin del  forEach para loop de todos los botones de la table
       });
-    });
-  }; //FIN DE POPULATE TABLA
-  populateAgenda();
-}); //FINDE LISTENER PARA AGENDA
+    }); //fin del  forEach para el snapshot
+    offsetCita();
+  });
+}
 
 //funcion para obtener el resto de horas que quedan sin apartar en el dia
-
 function horario() {
   const fecha = convertirFecha(new Date()); //fecha de hoy
   const listaHoras = document.getElementById('listaHoras');
@@ -474,7 +400,7 @@ function horario() {
   onSnapshot(q, snapshot => {
     fechaAgenda.innerHTML = formatearFecha(fecha);
     //aqui se itera sobre el cursor resultado (snapshot)
-    console.log('se disparo el onsnapshot de citas-horas');
+
     snapshot.forEach(doc => {
       let position = horas.indexOf(doc.data().hora);
       if (position >= 0) {
@@ -518,8 +444,7 @@ function bloquearHora(fechaBloquear, horaBloquear) {
     status: 'Bloqueada',
     createdAt: serverTimestamp(),
   });
-  //menuLinks[1].click();
-  window.scroll(0, 2);
+  window.scroll(0, 5);
 }
 
 //***********************SIDEBAR AGENDA *****************************************/
@@ -551,14 +476,14 @@ function hideNav() {
 }
 
 function deleteCita(id) {
-  const eliminar = confirm('Esta Seguro que quiere Eliminar este Paciente?');
+  const eliminar = confirm('Esta Seguro que desea eliminar esta Cita?');
   if (eliminar) {
     const docRef = doc(db, 'citas', id);
     deleteDoc(docRef)
       .then(result => {
-        //menuLinks[1].click();
-        alert('Cita Eliminada');
-        window.scroll(0, 2);
+        horario();
+        alert('Cita Eliminada OK!');
+        window.scroll(0, 10);
       })
       .catch(error => {
         alert('Error: ', error.message);
@@ -572,8 +497,38 @@ function getUsuarios() {
     snapshot.forEach(usuario => {
       let currentID = usuario.id;
       let appObj = { ...usuario.data(), ['id']: currentID };
-      usuarioWeb.push(appObj);
-      console.log('usuario agregado al array pacientesx');
+      usuariosWeb.push(appObj);
     });
   });
+}
+
+window.addEventListener('scroll', callbackFunc);
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function callbackFunc() {
+  console.log('callbackFunc=>executed');
+  var items = document.querySelectorAll('.timeline li');
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      items[i].classList.add('in-view');
+    } else {
+      items[i].classList.remove('in-view');
+    }
+    if (i == 0) {
+      items[i].classList.add('in-view');
+    }
+  } //end of for
+}
+
+function offsetCita() {
+  document.querySelector('#ul-timeline li:first-child').classList.add('in-view');
+  window.scroll(0, 5);
 }
